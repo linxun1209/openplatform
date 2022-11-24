2ac840bb832eef71c0b23146c3df5a56cdc74b09
linxun1209 <3056726345@qq.com>
2022-11-24 11:02:36 +0800

    first

---
+++ src/main/java/com/xingchen/openplatform/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package com.xingchen.openplatform.mapper;
+
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.xingchen.openplatform.model.entity.User;
+
+/**
+ * @Entity com.xingchen.openplatform.model.entity.User
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
